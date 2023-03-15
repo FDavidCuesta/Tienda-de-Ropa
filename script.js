f05847a2fe69b27b8fe80aea2b1971e485359cea
Fausto david Cuesta <davidFcuesta@gmail.com>
2023-03-15 12:41:43 -0300

    tienda Clothes Savage

--- script.js
+++ script.js
@@ -43,6 +43,15 @@ function apiFake(){
           };
         };
         checkCart() 
+        function updateMenu() {
+          if (document.getElementById('responsive-menu').checked == true) {
+            document.getElementById('menu').style.borderBottomRightRadius = '0';
+            document.getElementById('menu').style.borderBottomLeftRadius = '0';
+          }else{
+            document.getElementById('menu').style.borderRadius = '10px';
+          }
+        }
+        updateMenu()
 
   })
 }
